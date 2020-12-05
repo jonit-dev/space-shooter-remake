fa8241f27bad797f47f96b9e98e4b0b250dd4a43
Joao Paulo <joaopaulofurtado@live.com>
2020-12-04 21:20:43 -0800

    spawn enemy controller

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _moveSpeed = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _moveSpeed * Time.deltaTime);
+
+        if (transform.position.y <= -6)
+        {
+            transform.position = new Vector3(Random.Range(-9f, 9f), 6, 0); // reset initial position, with random x, if goes out of bounds on Y
+        }
+
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemySpawnController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnInterval());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator SpawnInterval()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            GameObject newEnemy = Instantiate(enemy, transform.position + new Vector3(Random.Range(-9f, 9f), 0, 0), Quaternion.identity);
+            // Debug.Log("Spawning enemy!");
+        }
+    }
+}
