495f466d41fcd50adb851275d48e0358efde35fa
Joao Paulo <joaopaulofurtado@live.com>
2020-12-04 20:48:44 -0800

    player laser shoot

---
+++ Assets/Scripts/Laser/LaserController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+
+    [SerializeField]
+    private int _moveSpeed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * _moveSpeed);
+
+        if (transform.position.y >= 6) // after it overflows the world boundary area, just remove it to increase game performance
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerLaser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLaser : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _nextFire = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.Space) && (Time.time > _nextFire))
+        {
+            _nextFire = Time.time + _fireRate;
+
+            // instantiate new laser beam, 0.8 Y offset to the top
+            GameObject laser = Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
+
+    }
+}
